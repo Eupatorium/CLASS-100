9c47b7441d5d60914c56b86beddc90b2e5259290
Eupatorium <62697127+Eupatorium@users.noreply.github.com>
2021-05-07 12:39:53 +0530

    Add files via upload

---
+++ 100 HW.py
@@ -0,0 +1,10 @@
+class atm(object): 
+    def __init__(self,cashwithdrawl,balanceenquiry):
+        self.cashwithdrawl = cashwithdrawl
+        self.balanceenquiry = balanceenquiry
+
+    def cashwithdrawl(self):
+        print("cash withdrawled")
+
+    def balanceenquiry(self):
+        print("balance enquiry")
